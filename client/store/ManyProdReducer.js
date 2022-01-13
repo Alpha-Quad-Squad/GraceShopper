c2ed3099e687298d7f44b268a3818834c1d6d71e
Jorgelly Ventura <jorgelly87@gmail.com>
2022-01-13 12:09:02 -0500

    fixed extra data and reducer variable issue

--- client/store/ManyProdReducer.js
+++ client/store/ManyProdReducer.js
@@ -80,72 +80,8 @@ export const deleteInventoryItem = (id) => {
   };
 };
 
-const initialState = [
-  {
-  id: 1,
-  itemImageUrl: 'https://edit.org/images/cat/book-covers-big-2019101610.jpg',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 14.99
-  },
-  {
-  id: 2,
-  itemImageUrl: 'https://m.media-amazon.com/images/I/41gr3r3FSWL.jpg',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 29.99
-  },
-  {
-  id: 3,
-  itemImageUrl: 'https://d1csarkz8obe9u.cloudfront.net/posterpreviews/contemporary-fiction-night-time-book-cover-design-template-1be47835c3058eb42211574e0c4ed8bf_screen.jpg?ts=1637012564',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 9.99
-  },
-  {
-  id: 4,
-  itemImageUrl: 'https://images-platform.99static.com//6ELqOlDZNAkWKAlKTT3XjDPSZ_c=/fit-in/590x590/projects-files/83/8342/834261/bc96e38c-765d-4031-a33f-b03eb49bca14.jpg',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 24.99
-  },
-  {
-  id: 5,
-  itemImageUrl: 'https://www.theyoungfolks.com/wp-content/uploads/2017/08/six-of-crows.jpg',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 49.99
-  },
-  {
-  id: 6,
-  itemImageUrl: 'https://microlancer.lancerassets.com/v2/services/9d/759cf0433411e695bffb04eef0d615/large__original_Spirit_s-Voice-Gothic-Book-Cover.jpg',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 18.99
-  },
-  {
-  id: 7,
-  itemImageUrl: 'https://www.adobe.com/express/create/cover/media_19e51036164ee5d6d263b7cd50578765583ffc3f2.jpeg?width=400&format=jpeg&optimize=medium',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 89.99
-  },
-  {
-  id: 8,
-  itemImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUiPGYANIq5JLuOUHEEQbK4pPRG2wKgLrhDNHnbMF6v7os1Rg5QT5INmOt7GSG211-iB8&usqp=CAU',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 44.99
-  },
-  {
-  id: 9,
-  itemImageUrl: 'https://m.media-amazon.com/images/I/513J3iCksDL.jpg',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 4.99
-  },
-  {
-  id: 10,
-  itemImageUrl: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRmD1v3g2gACOW7k3Q7XemLnYnOwD6ePlis7_kpVXqXLAB_nQ2KwEWKit5Dwn29zEHfj9g&usqp=CAU',
-  itemName: 'How To Become Awesome by Mars DeMartian',
-  itemPrice: 62.99
-  },
-
-];
-
 //Reducer
-export default (state = initialState, action) => {
+export default (state = {}, action) => {
   switch (action.type) {
     case SET_INVENTORYITEMS:
       return action.items;
@@ -160,7 +96,7 @@ export default (state = initialState, action) => {
       const deletedState = state.filter((item) => (
         item.id !== action.item.id
       ));
-      return newState;
+      return deletedState;
     default:
       return state;
   };
