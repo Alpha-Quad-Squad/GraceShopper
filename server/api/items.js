181b801281fd8aba1900ab97bce514007cc3322e
Lane Zimmerman <lane.zimmerman@gmail.com>
2022-01-13 19:13:02 -0500

    added the routes, tested POST

--- server/api/items.js
+++ server/api/items.js
@@ -19,3 +19,51 @@ router.get('/:productId', async (req, res, next) => {
     next(err)
   }
 })
+
+router.post('/', async (req, res, next) => {
+  try {
+    const newInventoryItem = await InventoryItem.create(
+      {
+        itemName: req.body.itemName,
+        itemDescription: req.body.itemDescription,
+        itemPrice: req.body.itemPrice,
+        itemImageUrl: req.body.itemImageUrl
+      }
+    )
+
+    // const testItem = await InventoryItem.create({
+    //   itemName: "The Institute by Stephen King",
+    //   itemDescription:
+    //     "The Institute is a science fiction-horror thriller novel by American author Stephen King, published on September 10, 2019, by Scribner. The book follows twelve-year-old genius Luke Ellis.",
+    //   itemPrice: 13.93,
+    //   itemImageUrl:
+    //     "https://images-na.ssl-images-amazon.com/images/I/81pA6-hv+2L.jpg",
+    // })
+
+
+    res.send(newInventoryItem)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.delete('/:productId', async (req, res, next) => {
+  try {
+    const deletedInventoryItem = await InventoryItem.getItem(req.params.productId)
+    await deletedInventoryItem.destroy();
+    res.send(deletedInventoryItem)
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.put('/:productId', async (req, res, next) => {
+  try {
+    const updatedInventoryItem =  await InventoryItem.getItem(req.params.productId)
+    await updatedInventoryItem.update(req.body)
+    res.send(updatedInventoryItem)
+  }
+  catch (error) {
+    next(error)
+  }
+})
