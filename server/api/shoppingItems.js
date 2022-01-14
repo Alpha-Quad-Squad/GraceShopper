3edae2c3ff57a4cf091342d24f4912517ec5414a
Liz Yoder <EAHYoder@gmail.com>
2022-01-14 14:46:10 -0600

    initial commit

--- server/api/shoppingItems.js
+++ server/api/shoppingItems.js
@@ -1,20 +1,23 @@
-const router = require('express').Router()
-const { models: { InventoryItem, ShoppingItem, Purchase }} = require('../db')
-module.exports = router
+const router = require("express").Router();
+const {
+  models: { InventoryItem, ShoppingItem, Purchase },
+} = require("../db");
+module.exports = router;
 
-router.get('/cart/:userId', async(req,res,next) => {
-    try {
-        console.log(req.params.userId)
-        const id = req.params.userId
-        const userCart = await Purchase.findAll({
-            where: {
-                userId: id,
-                status: "cart"
-            },
-            include: InventoryItem
-        });
-        res.json(userCart)
-    } catch(err) {
-        next(err)
-    }
-})
+router.get("/cart/:userId", async (req, res, next) => {
+  try {
+    console.log(req.params.userId);
+    const id = req.params.userId;
+    const userCart = await Purchase.findAll({
+      where: {
+        userId: id,
+        status: "cart",
+      },
+      include: InventoryItem,
+    });
+    res.json(userCart);
+    //working on this
+  } catch (err) {
+    next(err);
+  }
+});
