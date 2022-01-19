1ab25e0b160adc79176959848fc44c5d5ddae190
Liz Yoder <EAHYoder@gmail.com>
2022-01-19 14:43:18 -0600

    cart is no longer doubling and log in is working correctly

--- client/store/auth.js
+++ client/store/auth.js
@@ -19,6 +19,7 @@ const setAuth = (auth) => ({ type: SET_AUTH, auth });
  * THUNK CREATORS
  */
 export const me = () => async (dispatch) => {
+  console.log("me thunk is starting");
   const token = window.localStorage.getItem(TOKEN);
   if (token) {
     const { data: auth } = await axios.get("/auth/me", {
@@ -28,47 +29,95 @@ export const me = () => async (dispatch) => {
     });
 
     const { id } = auth;
-    //check if there are items in a guest cart that need to be added to the backend cart for this user
-    const frontEndCart = JSON.parse(window.localStorage.getItem(CART));
+    // //check if there are items in a guest cart that need to be added to the backend cart for this user
+    // const frontEndCart = JSON.parse(window.localStorage.getItem(CART));
 
-    const { data: backEndCart } = await axios.get(`/api/cart/${id}`, {
-      headers: {
-        authorization: token,
-      },
-    });
+    // const { data: backEndCart } = await axios.get(`/api/cart/${id}`, {
+    //   headers: {
+    //     authorization: token,
+    //   },
+    // });
 
-    if (frontEndCart) {
-      //go add the frontend cart to backEnd, and update the cart in redux store with the new information from the backend.
-      await frontEndCart.forEach(async (frontEndProduct) => {
-        //get qty of the item that is already in this user's back end cart
-        let [productInBackEndCart] = backEndCart.filter(
-          (backEndProduct) => backEndProduct.id === frontEndProduct.id
-        );
-        let backEndQty = 0;
-        if (productInBackEndCart) {
-          backEndQty = productInBackEndCart.qty;
-        }
-        let newQuantity = backEndQty + frontEndProduct.qty;
-        await dispatch(goAddShoppingItem(frontEndProduct, id, newQuantity));
-      });
-    }
+    // if (frontEndCart) {
+    //   //go add the frontend cart to backEnd, and update the cart in redux store with the new information from the backend.
+    //   await frontEndCart.forEach(async (frontEndProduct) => {
+    //     //get qty of the item that is already in this user's back end cart
+    //     let [productInBackEndCart] = backEndCart.filter(
+    //       (backEndProduct) => backEndProduct.id === frontEndProduct.id
+    //     );
+    //     let backEndQty = 0;
+    //     if (productInBackEndCart) {
+    //       backEndQty = productInBackEndCart.qty;
+    //     }
+    //     let newQuantity = backEndQty + frontEndProduct.qty;
+    //     await dispatch(goAddShoppingItem(frontEndProduct, id, newQuantity));
+    //   });
+    // }
 
     //get backend cart for this user.
     dispatch(fetchCart(id));
     dispatch(setAuth(auth));
+    console.log("me thunk is ending");
   }
 };
 
 export const authenticate =
   (username, password, method, email) => async (dispatch) => {
     try {
+      console.log("authenticate thunk is starting");
+      //get a token based on log in information
       const res = await axios.post(`/auth/${method}`, {
         username,
         password,
         email,
       });
+
+      console.log("token from authenticate thunk", res.data.token);
+      //put the token in local storage
       window.localStorage.setItem(TOKEN, res.data.token);
-      dispatch(me());
+      //dispatch(me());
+
+      //get the user info based on their token
+      const { data: auth } = await axios.get("/auth/me", {
+        headers: {
+          authorization: res.data.token,
+        },
+      });
+      dispatch(setAuth(auth));
+      console.log("auth info from authenticate thunk", auth);
+      const { id } = auth;
+
+      //check if there are items in a guest cart that need to be added to the backend cart for this user
+      const frontEndCart = JSON.parse(window.localStorage.getItem(CART));
+      console.log("frontend cart from authenticate thunk", frontEndCart);
+
+      //check if there are items in the backend cart that need to be merged with front end cart.
+      const { data: backEndCart } = await axios.get(`/api/cart/${id}`, {
+        headers: {
+          authorization: res.data.token,
+        },
+      });
+
+      console.log("backend cart from authenticate thunk", backEndCart);
+
+      if (frontEndCart) {
+        //go add the frontend cart to backEnd, then update the cart in redux store with the new information from the backend.
+        await frontEndCart.forEach(async (frontEndProduct) => {
+          //get qty of the item that is already in this user's back end cart
+          let [productInBackEndCart] = backEndCart.filter(
+            (backEndProduct) => backEndProduct.id === frontEndProduct.id
+          );
+          let backEndQty = 0;
+          if (productInBackEndCart) {
+            backEndQty = productInBackEndCart.qty;
+          }
+          let newQuantity = backEndQty + frontEndProduct.qty;
+          await dispatch(goAddShoppingItem(frontEndProduct, id, newQuantity));
+        });
+      }
+
+      //get backend cart for this user.
+      dispatch(fetchCart(id));
     } catch (authError) {
       return dispatch(setAuth({ error: authError }));
     }
